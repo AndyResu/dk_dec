188ea0fe9c1d563beaa24ba686f63311e212c57c
real name genij <zgxzgx@gmail.com>
2021-12-16 23:02:32 -0600

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dk_dec
 Dastardly Kangs December
+
+Following: https://www.conventionalcommits.org/en/v1.0.0/
---
+++ code/dkdGame.cs
@@ -0,0 +1,28 @@
+﻿
+using Sandbox;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace dkdGame
+{
+	public partial class dkdGame : Sandbox.Game
+	{
+		public dkdGame()
+		{
+
+		}
+
+		public override void ClientJoined( Client client )
+		{
+			base.ClientJoined( client );
+
+			var player = new dkdPlayer();
+			client.Pawn = player;
+
+			player.Respawn();
+		}
+	}
+
+}
