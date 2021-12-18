bf5ae51c389ceeb2db1036c5d217aec5ed611bdb
real name genij <zgxzgx@gmail.com>
2021-12-18 11:21:54 -0600

    feat: snowball spawn

--- code/dkdGame.cs
+++ code/dkdGame.cs
@@ -1,5 +1,4 @@
-﻿
-using Sandbox;
+﻿using Sandbox;
 using Sandbox.UI.Construct;
 using System;
 using System.IO;
@@ -9,8 +8,6 @@ namespace dkdGame
 {
 	public partial class dkdGame : Sandbox.Game
 	{
-		public Vector3 snowballSpawnerPos = new Vector3(-468,700,1168);
-
 		public dkdGame()
 		{
 			if ( IsServer )
@@ -32,19 +29,11 @@ namespace dkdGame
 		[Event.Tick]
 		public void snowballSpawn()
 		{
-			// console.WriteLine();
-			var time = 10000.00;
 			var rng = new Random();
-			if(time <= 0){
-				var snowball = new ModelEntity();
-				snowball.SetModel("models/things_from_kabubu2/snowball.vmdl");
-				snowball.Position = new Vector3(rng.Next(-32, 33), rng.Next(-32, 33), 0) + snowballSpawnerPos;
-				// x and y are the ground coords
-				snowball.Rotation = Rotation.LookAt( Vector3.Random.Normal );
-				snowball.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
-				time = 10000;
-			} else {
-				time -= Time.Delta;
+			var rngVal = rng.Next(1,300);
+			if(rngVal <= 1.0){
+				var snowball = new Snowball();
+				Log.Info("SNOWBALL!");
 			}
 		}
 	}
---
+++ code/snowball.cs
@@ -0,0 +1,79 @@
+﻿using Hammer;
+using Sandbox;
+using Sandbox.Internal;
+using System.Linq;
+using Sandbox.UI.Construct;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+[Library("snowball", Description = "Do not touch!")]
+[EditorModel("models/editor/playerstart.vmdl", staticGreen: 100, FixedBounds = true)]
+[EntityTool("Snowball", "dkdGame", "Do not touch!")]
+[BoundsHelper("mins", "maxs", false, true)]
+internal partial class Snowball : ModelEntity
+{
+	[Property("mins", Title = "Lower Corner of bounding box for the snowball.")]
+	[DefaultValue("-35 -35 -35")]
+	[Net]
+	public Vector3 Mins { get; set; } = new Vector3(-35, -35, -35);
+	[Property("maxs", Title = "Upper Corner of bounding box for the snowball.")]
+	[DefaultValue("35 35 35")]
+	[Net]
+	public Vector3 Maxs { get; set; } = new Vector3(35, 35, 35);
+	public Vector3 snowballSpawnerPos = new Vector3(608,736,288);
+	public Random rng = new Random();
+
+	public ModelEntity Snowballe;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Snowballe = new ModelEntity("models/things_from_kabubu2/snowball.vmdl");
+		Snowballe.SetupPhysicsFromModel(PhysicsMotionType.Dynamic, false);
+		var bounds = new BBox(Position + Mins, Position + Maxs);
+		// Snowballe.Position = bounds.ClosestPoint(new Vector3(rng.Next(-25, 26), rng.Next(-25, 26), 0) + snowballSpawnerPos);
+		Snowballe.Position = new Vector3(rng.Next(-10, 11), rng.Next(-10, 11), 0) + snowballSpawnerPos;
+		Snowballe.Rotation = Rotation.LookAt(Vector3.Random.Normal);
+
+		Transmit = TransmitType.Always;
+		CollisionGroup = CollisionGroup.Trigger;
+		EnableSolidCollisions = false;
+		EnableTouch = true;
+	}
+
+	public override void ClientSpawn()
+	{
+		// base.ClientSpawn();
+
+		// would be fun to have the snowball gain parts of things as it hits things.
+	}
+
+	public override void StartTouch( Entity other )
+	{
+		Log.Info(other);
+		base.StartTouch( other );
+		//if ( other is not dkdPlayer pl ) return;
+
+		// pl.kill() 
+		// or reduce hp, idk
+	}
+
+	public override void EndTouch( Entity other )
+	{
+		Log.Info("FUCK " + other);
+		base.EndTouch( other );
+		//if ( other is not dkdPlayer pl ) return;
+	}
+
+	[Event.Frame]
+	private void OnFrame()
+	{
+		//DebugOverlay.Box( Position + Mins, Position + Maxs, color );
+		/*if (Snowballe.getPos().z <= 0)
+		{
+			Snowballe.Remove();
+		}*/
+	}
+}
